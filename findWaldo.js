45866997845229e99d4dc7147b6af4e7c57e9d1f
shane <navysupra@gmail.com>
2022-07-28 19:53:17 +0000

    refactored as per step 2

--- findWaldo.js
+++ findWaldo.js
@@ -1,11 +1,10 @@
 // Functions
 const findWaldo = (names,found) => {
-  for (let i = 0; i < names.length; i++) {
-    const name = names[i];
-    if (name === 'Waldo') {
-      found(i);
+  names.forEach((name, index) => {
+    if (name === `Waldo`) {
+      found(index);
     }
-  }
+  });
 };
 const actionWhenFound = (index) => {
   console.log(`Found Waldo at index ${index}`);
